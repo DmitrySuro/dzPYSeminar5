a3978f129b5057f031bdf6a3baf631658790b86a
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-10-11 22:09:49 +0300

    Разбор решений

---
+++ task3.py
@@ -0,0 +1,135 @@
+import os
+
+
+def create_field():
+    # ONLY SQUERES !!!!
+    return [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+def get_user_name(message:str, users: dict):
+    while (True):
+        name = input(message)
+        if name in users:
+            print("Имя уже занято, введите псевдоним!")
+        else:
+            users[name] = list()
+            break
+
+def is_win(field: list):
+    sum_to_win = len(field)
+    row_sum = 0
+    column_sum = 0
+    diagonal_left_sum = 0
+    diagonal_right_sum = 0
+    is_not_empty = True
+
+    for i in range(len(field)):
+        for j in range(len(field[i])):
+            row_sum += field[i][j]
+            column_sum += field[j][i]
+            
+            if(field[i][j] == 0):
+                is_not_empty = False
+            if i == j:
+                diagonal_left_sum += field[i][j]
+            if len(field[i]) -1 -i == j:
+                diagonal_right_sum += field[i][j]
+        if abs(row_sum) == sum_to_win or abs(column_sum) == sum_to_win:
+            return 1
+        else:
+            row_sum = 0
+            column_sum = 0
+            
+    
+    if abs(diagonal_left_sum) == sum_to_win or abs(diagonal_right_sum) == sum_to_win:
+        return 1
+    
+    if is_not_empty:
+        return -1
+    
+    return 0
+
+def get_mark(val: int):
+    marks = {
+        -1: "O",
+        1: "X",
+        0: " "
+    }
+    
+    if val in marks:
+        return marks[val]
+    else:
+        return " "
+
+def print_field(field: dict):
+    count = 1
+    
+    for i in range(len(field)):
+        print()
+        for j in range(len(field[i])):
+            if 0 == j or j == len(field[i]):
+                print(" ", end="")
+            else:
+                print("|", end="")
+            print(f"  {get_mark(field[i][j])}   ", end="")
+            count += 1
+        print("\n")
+        for c in range(count - len(field), count):
+            print(f"---{c}---", end="")
+        print()
+        
+def handle_user_input(field: list, sign: int, name: str, users: dict):
+    max_number = len(field) * len(field[0])
+    while (True):
+        try:
+            cell_number = int(input(f"{name} укажите номер ячейки: "))
+            cell_number = cell_number - 1
+            if 0 <= cell_number < max_number:
+                change_cell(cell_number, sign, field)
+                users[name].append(cell_number)
+                return cell_number
+            else:
+                raise ValueError("Wrong number")
+        except:
+            print("Не допустимое знаЧение, повторите ввод!")
+            continue
+
+       
+def change_cell(cell_number: int, sign: int, field: list):
+    row = cell_number // 3
+    col = cell_number % 3
+    
+    if (field[row][col] != 0):
+        raise ValueError("Не пустое поле!")
+  
+    field[row][col] = sign
+
+
+def tik_tak_game():
+    users = dict()
+    get_user_name("Введите имя первого игрока ", users)
+    get_user_name("Введите имя второго игрока ", users)
+    
+    field = create_field()
+    game = is_win(field)
+    print_field(field)
+    while (game == 0):
+        sign = 1
+        for name, _ in users.items():
+            handle_user_input(field, sign, name, users)
+            clear = lambda: os.system('clear')
+            clear()
+            print_field(field)
+            game = is_win(field)
+            if game == 1:
+                print(f"Выиграл игрок {name}")
+                return
+            elif game == -1:
+                print("Ничья")
+                return
+            sign *= -1
+    
+tik_tak_game()
---
+++ task4.py
@@ -0,0 +1,43 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+from collections import Counter
+from curses.ascii import isdigit
+
+def unzip(compressed_string:str):
+    new_string = ""
+    digit = ""
+    for i in range(len(compressed_string)):
+        if compressed_string[i].isdigit():
+            digit += compressed_string[i]
+        else:
+            new_string += compressed_string[i] * int(digit)
+            digit = ""
+            
+    return new_string
+        
+    
+def rle2(string: str):
+    new_string = ""
+    last_symmbol = string[0]
+    count = 1
+    
+    for i in range(1, len(string)):
+        if string[i] == last_symmbol:
+            count += 1
+            continue
+        else:
+            new_string += f"{count}{last_symmbol}"
+            last_symmbol = string[i]
+            count = 1
+            
+    new_string += f"{count}{last_symmbol}"
+    
+    return new_string
+
+in_string = "wwwwwwwwwwwwwwasdasdasdasdasdasdoooooooqweqwadaaaaaaaa"
+print(in_string)
+compressed = rle2(in_string)
+print(compressed)
+unpack = unzip(compressed)
+print(unpack)
+print(unpack == in_string)
